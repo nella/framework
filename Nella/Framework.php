5dfd5509fe890b4b98d1cf4a733ebfa94514bfa3
Patrik Votocek <patrik@votocek.cz>
2011-01-09 21:31:12 +0100

    Add basic files (Nella\Framework + tests bootstrap)

---
+++ Nella/Framework.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nellacms.com
+ */
+
+namespace Nella;
+
+/**
+ * The Nella Framework;
+ *
+ * @author	Patrik Votoček
+ */
+final class Framework
+{
+	/**#@+ Nella Framework version identification */
+	const NAME = 'Nella Framework';
+
+	const VERSION = '2.0-dev';
+
+	const REVISION = '$WCREV$ released on $WCDATE$';
+	/**#@-*/
+	
+	/**
+	 * Static class - cannot be instantiated.
+	 */
+	final public function __construct()
+	{
+		throw new \LogicException("Cannot instantiate static class " . get_called_class());
+	}
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nellacms.com
+ */
+
+// Load Nette Framework
+require_once __DIR__ . "/../../Nette/loader.php";
+
+// Init Nette Framework robot loader
+$loader = new Nette\Loaders\RobotLoader;
+$loader->setCacheStorage(new Nette\Caching\MemoryStorage);
+$loader->addDirectory(__DIR__ . "/../Nella");
+$loader->addDirectory(__DIR__);
+$loader->register();
+
+define('APP_DIR', __DIR__);
+define('NELLA_DIR', __DIR__ . "/../Nella");
