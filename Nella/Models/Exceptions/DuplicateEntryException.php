6da9621dc0d537bee3a6759bc32787c0bf1ac432
Patrik Votocek <patrik@votocek.cz>
2011-02-10 09:53:08 +0100

    Add model Exceptions

---
+++ Nella/Models/Exceptions/DuplicateEntryException.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nellacms.com
+ */
+
+namespace Nella\Models;
+
+/**
+ * Duplicate column value exception
+ *
+ * @author	Patrik Votoček
+ */
+class DuplicateEntryException extends \InvalidStateException implements IException
+{
+	/** @var string */
+	private $name;
+	
+	/**
+	 * @param string
+	 * @param string
+	 * @param int
+	 * @param \Exception
+	 */
+	public function __construct($name, $message = NULL, $code = NULL, \Exception $previous = NULL)
+	{
+		parent::__construct($message, $code, $previous);
+		$this->name = $name;
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+}
---
+++ Nella/Models/Exceptions/IException.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nellacms.com
+ */
+
+namespace Nella\Models;
+
+/**
+ * Interface for Nella models exceptions
+ *
+ * @author	Patrik Votoček
+ */
+interface IException
+{
+	/**
+	 * @param string
+	 * @param string
+	 * @param int
+	 * @param \Exception
+	 */
+	public function __construct($name, $message = NULL, $code = NULL, \Exception $previous = NULL);
+	
+	/**
+	 * @return string
+	 */
+	public function getName();
+}
