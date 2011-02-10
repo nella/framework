12fb9f11b5c002539ea815c8aae1ccbe1c77f4b4
Patrik Votocek <patrik@votocek.cz>
2011-02-10 09:53:55 +0100

    Add image

---
+++ Nella/Image.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nellacms.com
+ */
+
+namespace Nella;
+
+/**
+ * Basic manipulation with images
+ *
+ * @author	Patrik Votoček
+ */
+class Image extends \Nette\Image
+{
+	/**
+	 * @param int
+	 * @param int
+	 * @return Image
+	 */
+	public function resizeAndCrop($width, $height)
+	{
+		return $this->resize($width, $height, self::FILL | self::ENLARGE)->crop('50%', '50%', $width, $height);
+	}
+}
