19dbe45be12d8be3a18581659f186402a35c9376
Patrik Votocek <patrik@votocek.cz>
2011-03-03 01:14:05 +0100

    tests bootstrap refactoring

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,22 +7,41 @@
  * This source file is subject to the GNU Lesser General Public License. For more information please see http://nella-project.org
  */
 
+// Define dirs constants
+define('APP_DIR', __DIR__);
+if (file_exists(__DIR__ . "/../src/Nella")) {
+	define('NELLA_FRAMEWORK_DIR', APP_DIR . "/../src/Nella");
+} else {
+	define('NELLA_FRAMEWORK_DIR', APP_DIR . "/../Nella");
+}
+if (file_exists(APP_DIR . "/../dependency")) {
+	define('DEPENDENCY_DIR', APP_DIR . "/../dependency");
+} else {
+	define('DEPENDENCY_DIR', APP_DIR . "/../../dependency");
+}
+
 // Load Nette Framework
-require_once __DIR__ . "/../dependency/Nette/loader.php";
+require_once DEPENDENCY_DIR . "/Nette/loader.php";
 
+// Setup Nette profiler
 Nette\Debug::enable(Nette\Debug::PRODUCTION);
-Nette\Debug::$logDirectory = __DIR__;
-
-Nette\Debug::$maxDepth = 10;
+Nette\Debug::$logDirectory = APP_DIR;
 Nette\Debug::$maxLen = 4096;
+
+/**
+ * @param mixed
+ * @param string
+ */
+function fdump($var, $file = NULL)
+{
+	file_put_contents($file ?: __DIR__ . "/dump.html", "<code><pre>" . \Nette\Debug::dump($var, true) . "</pre></code>", FILE_APPEND);
+}
 
 // Init Nette Framework robot loader
 $loader = new Nette\Loaders\RobotLoader;
 $loader->setCacheStorage(new Nette\Caching\MemoryStorage);
-$loader->addDirectory(__DIR__ . "/../dependency");
-$loader->addDirectory(__DIR__ . "/../src/Nella");
-$loader->addDirectory(__DIR__);
+$loader->addDirectory(DEPENDENCY_DIR);
+$loader->addDirectory(NELLA_FRAMEWORK_DIR);
+$loader->addDirectory(APP_DIR);
 $loader->register();
 
-define('APP_DIR', __DIR__);
-define('NELLA_FRAMEWORK_DIR', __DIR__ . "/../src/Nella");
