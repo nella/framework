49dc48a8c9e3555e522909ed064ad803f3e7bfcd
Patrik Votocek <patrik@votocek.cz>
2011-04-11 13:16:06 +0200

    Image: add extToType and typeToExt

--- Nella/Image.php
+++ Nella/Image.php
@@ -25,4 +25,57 @@ class Image extends \Nette\Image
 	{
 		return $this->resize($width, $height, self::FILL | self::ENLARGE)->crop('50%', '50%', $width, $height);
 	}
+	
+	/**
+	 * @param string
+	 * @return int
+	 */
+	public static function extToType($type)
+	{
+		switch ($type) {
+			case 'png':
+				return static::PNG;
+				break;
+			case 'gif':
+				return static::GIF;
+				break;
+			default:
+				return static::JPEG;
+				break;
+		}
+	}
+	
+	/**
+	 * @param int
+	 * @return string
+	 */
+	public static function typeToExt($type)
+	{
+		switch ($type) {
+			case static::PNG:
+				return 'png';
+				break;
+			case static::GIF:
+				return 'gif';
+				break;
+			default:
+				return 'jpg';
+				break;
+		}
+	}
+	
+	/**
+	 * Outputs image to browser.
+	 * @param  int  image type
+	 * @param  int  quality 0..100 (for JPEG and PNG)
+	 * @return bool TRUE on success or FALSE on failure.
+	 */
+	public function send($type = self::JPEG, $quality = NULL)
+	{
+		if (is_string($type)) {
+			$type = self::extToType($type);
+		}
+		
+		return parent::send($type, $quality);
+	}
 }
