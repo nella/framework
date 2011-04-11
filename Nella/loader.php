64be1ba5a0c012e4a917a5b922c3e5ff1ca0e84e
Patrik Votocek <patrik@votocek.cz>
2011-04-11 10:31:01 +0200

    set session and upload dir

--- Nella/loader.php
+++ Nella/loader.php
@@ -13,13 +13,18 @@ namespace Nella;
 if (!defined('NETTE')) {
 	require_once __DIR__ . "/../Nette/loader.php";
 }
- 
+
 /**
  * Load and configure Nella Framework
  */
 define('NELLA_FRAMEWORK', TRUE);
 define('NELLA_FRAMEWORK_DIR', __DIR__);
 define('NELLA_FRAMEWORK_VERSION_ID', 20000); // v2.0.0
+
+// Set file upload temp dir
+ini_set('upload_tmp_dir', TEMP_DIR . "/uploaded");
+// Set session dir
+ini_set('session.save_path', TEMP_DIR . "/sessions");
 
 /** #@+ Base control flash messages class */
 const FLASH_SUCCESS = "success";
