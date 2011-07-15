a57da2e5fb1d649f95ea7d30923ac1793cbb6932
Michael Moravec <mail@majkl578.cz>
2011-07-15 22:36:16 +0200

    CallbackBarPanel: Fixed request processing - uses Application::$onStartup

--- Nella/Diagnostics/CallbackBarPanel.php
+++ Nella/Diagnostics/CallbackBarPanel.php
@@ -18,7 +18,7 @@ use Nette\Environment;
  */
 final class CallbackBarPanel extends \Nette\Object implements \Nette\Diagnostics\IBarPanel
 {
-	const VERSION = "1.7",
+	const VERSION = "1.8",
 		XHR_HEADER = "X-Nella-Callback-Panel";
 	/** @var \Nette\DI\IContainer */
 	private $container;
@@ -38,6 +38,9 @@ final class CallbackBarPanel extends \Nette\Object implements \Nette\Diagnostics
 
 		$this->container = $container;
 
+		//schedule processing on application startup
+		$container->application->onStartup[] = callback($this, 'processRequest');
+
 		$this->initDefaultsCallbacks();
 
 		static::$registered = TRUE;
@@ -55,8 +58,8 @@ final class CallbackBarPanel extends \Nette\Object implements \Nette\Diagnostics
 			$robotLoader->rebuild();
 		});
 	}
-	
-	protected function processRequest()
+
+	public function processRequest()
 	{
 		$httpRequest = $this->container->httpRequest;
 		if ($httpRequest->getHeader(static::XHR_HEADER)) {
@@ -98,14 +101,12 @@ final class CallbackBarPanel extends \Nette\Object implements \Nette\Diagnostics
 
 	/**
 	 * Renders HTML code for custom tab
-	 * 
+	 *
 	 * @return string
 	 * @see Nette\IDebugPanel::getTab()
 	 */
 	public function getTab()
 	{
-		$this->processRequest();
-		
 		return '<span title="Callbacks">
 			<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAK8AAACvABQqw0mAAAABh0RVh0U29mdHdhcmUAQWRvYmUgRmlyZXdvcmtzT7MfTgAAAY9JREFUOI2lkj1rVUEQhp93d49XjYiCUUFtgiBpFLyWFhKxEAsbGy0ErQQrG/EHCII/QMTGSrQ3hY1FijS5lQp2guBHCiFRSaLnnN0di3Pu9Rpy0IsDCwsz8+w776zMjP+J0JV48nrufMwrc2AUbt/CleMv5ycClHH1UZWWD4MRva4CByYDpHqjSgKEETcmHiHmItW5STuF/FfAg8HZvghHDDMpkKzYXScPgFcx9XBw4WImApITn26cejEAkJlxf7F/MOYfy8K3OJGtJlscKsCpAJqNGRknd+jO6TefA8B6WU1lMrBZ6fiE1R8Zs7hzVJHSjvJnNMb/hMSmht93IYIP5Qhw99zSx1vP+5eSxZmhzpzttmHTbcOKk+413Sav4v3J6ZsfRh5sFdefnnhr2Gz75rvHl18d3aquc43f1/BjaN9V1wn4tq6eta4LtnUCQuPWHmAv0AOKDNXstZln2/f3zgCUX8oFJx1zDagGSmA1mn2VmREk36pxw5NgzVqDhOTFLhjtOgMxmqVOE/81fgFilqPyaom5BAAAAABJRU5ErkJggg==">
 			callback
@@ -114,7 +115,7 @@ final class CallbackBarPanel extends \Nette\Object implements \Nette\Diagnostics
 
 	/**
 	 * Renders HTML code for custom panel
-	 * 
+	 *
 	 * @return string
 	 * @see Nette\IDebugPanel::getPanel()
 	 */
@@ -126,7 +127,7 @@ final class CallbackBarPanel extends \Nette\Object implements \Nette\Diagnostics
 		require_once __DIR__ . "/templates/bar.callback.panel.phtml";
 		return ob_get_clean();
 	}
-	
+
 	/**
 	 * @param \Nette\Diagnostics\Bar
 	 * @param \Nette\DI\IContainer
