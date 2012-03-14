de6f8bf6e8bb59155379404e89d828a52995ed3d
Patrik Votocek <patrik@votocek.cz>
2012-03-14 14:42:25 +0100

    Model: add exceptions

---
+++ Nella/Model/InvalidEntityException.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nella-project.org
+ */
+
+namespace Nella\Model;
+
+/**
+ * Ivalid entity state exception
+ *
+ * @author	Patrik Votoček
+ */
+class InvalidEntityException extends Exception
+{
+	/** @var array */
+	private $errors;
+
+	/**
+	 * @param string
+	 * @param array
+	 */
+	public function __construct($message, array $errors)
+	{
+		parent::__construct($message);
+		$this->errors = $errors;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+}
