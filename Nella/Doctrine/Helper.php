de8b0a54197c46469ced07628fb06fd6288555a5
Patrik Votocek <patrik@votocek.cz>
2012-03-14 14:44:14 +0100

    Doctrine: add helper

---
+++ Nella/Doctrine/Helper.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nella-project.org
+ */
+
+namespace Nella\Doctrine;
+
+/**
+ * Doctrine helper
+ *
+ * @author	Patrik Votoček
+ */
+class Helper extends \Nette\Object
+{
+	public function __construct()
+	{
+		throw new \Nette\StaticClassException;
+	}
+
+	/**
+	 * @param \PDOException
+	 * @throws \Nella\Model\DuplicateEntryException
+	 * @throws \Nella\Model\EmptyValueException
+	 * @throws \Nella\Model\Exception
+	 */
+	public static function convertException(\PDOException $e)
+	{
+		$info = $e->errorInfo;
+		if ($info[0] == 23000 && $info[1] == 1062) { // unique fail
+			// @todo how to detect column name ?
+			throw new \Nella\Model\DuplicateEntryException($e->getMessage(), NULL, $e);
+		} elseif ($info[0] == 23000 && $info[1] == 1048) { // notnull fail
+			// @todo convert table column name to entity column name
+			$name = substr($info[2], strpos($info[2], "'") + 1);
+			$name = substr($name, 0, strpos($name, "'"));
+			throw new \Nella\Model\EmptyValueException($e->getMessage(), $name, $e);
+		} else { // other fail
+			throw new \Nella\Model\Exception($e->getMessage(), 0, $e);
+		}
+	}
+}
---
+++ tests/cases/Doctrine/HelperTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nellacms.com
+ */
+
+namespace NellaTests\Doctrine;
+
+use Nella\Doctrine\Helper,
+	Nella\Model;
+
+class HelperTest extends \Nella\Testing\TestCase
+{
+	public function dataConvertException()
+	{
+		$dupl1 = new \PDOException('foo', 23000);
+		$dupl1->errorInfo = array(23000, 1062);
+
+		$empty1 = new \PDOException('bar', 23000);
+		$empty1->errorInfo = array(23000, 1048, "error in 'test'");
+
+		$other = new \PDOException('baz', 23000);
+		$other->errorInfo = array(23000, 999999);
+
+		return array(
+			array($dupl1, new Model\DuplicateEntryException($dupl1->getMessage(), NULL, $dupl1)),
+			array($empty1, new Model\EmptyValueException($empty1->getMessage(), 'test', $empty1)),
+			array($other, new Model\Exception($other->getMessage(), NULL, $other)),
+		);
+	}
+
+	/**
+	 * @dataProvider dataConvertException
+	 */
+	public function testConvertException($exception, $expected)
+	{
+		try {
+			Helper::convertException($exception);
+		} catch (\Nella\Model\Exception $e) {
+			$this->assertEquals($expected, $e); // @todo message
+		}
+	}
+}
