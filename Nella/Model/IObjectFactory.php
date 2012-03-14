5b7f4d8eb29e1ff02e8319acf6ed90132214fcba
Patrik Votocek <patrik@votocek.cz>
2012-03-14 17:49:54 +0100

    Model: add IObjectFactory

---
+++ Nella/Model/IObjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the Nella Framework.
+ *
+ * Copyright (c) 2006, 2011 Patrik Votoček (http://patrik.votocek.cz)
+ *
+ * This source file is subject to the GNU Lesser General Public License. For more information please see http://nella-project.org
+ */
+
+namespace Nella\Model;
+
+/**
+ * Object Factory interface
+ *
+ * @author	Patrik Votoček
+ */
+interface IObjectFactory
+{
+	/**
+	 * @param array
+	 * @return object
+	 */
+	public function create(array $values = array());
+}
