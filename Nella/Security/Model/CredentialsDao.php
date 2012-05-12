c06947b3ea714bee9c1c89035b8fc9bc11b49eec
Patrik Votocek <patrik@votocek.cz>
2012-05-12 03:01:12 +0200

    Security: refactoring credentials dao

--- Nella/Security/Model/CredentialsDao.php
+++ Nella/Security/Model/CredentialsDao.php
@@ -17,6 +17,20 @@ namespace Nella\Security\Model;
 class CredentialsDao extends \Nella\Doctrine\Dao
 {
 	/**
+	 * @param array
+	 * @return CredentialsEntity
+	 */
+	public function create(array $values = array())
+	{
+		$values['identity'] = new IdentityEntity;
+		$ref = $this->em->getClassMetadata($this->repository->getClassName())->getReflectionClass();
+		if (empty($values)) {
+			return $ref->newInstance();
+		}
+		return $ref->newInstanceArgs(array($values['identity']));
+	}
+
+	/**
 	 * @param string
 	 * @return CredentialsEntity|NULL
 	 */
@@ -27,5 +41,35 @@ class CredentialsDao extends \Nella\Doctrine\Dao
 		} else {
 			return $this->repository->findOneByUsername($credential);
 		}
+	}
+
+	/**
+	 * @param string
+	 * @return CredentialsEntity|NULL
+	 */
+	public function findOneByUsername($username)
+	{
+		return $this->repository->findOneByUsername($username);
+	}
+
+	/**
+	 * @param string
+	 * @return CredentialsEntity|NULL
+	 */
+	public function findOneByEmail($email)
+	{
+		return $this->repository->findOneByEmail($email);
+	}
+
+	/**
+	 * @param object
+	 * @param bool
+	 */
+	public function save($entity, $withoutFlush = self::FLUSH)
+	{
+		if (isset($entity->identity)) {
+			parent::save($entity->identity, self::NO_FLUSH);
+		}
+		return parent::save($entity, $withoutFlush);
 	}
 }
