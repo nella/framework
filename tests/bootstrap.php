7bfa9121835bb683bac07ab1425ce96408e272fc
Patrik Votocek <patrik@votocek.cz>
2012-05-30 09:21:44 +0200

    update composer config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -19,7 +19,7 @@ $params = array(
 );
 
 require_once $params['libsDir'] . "/nette/nette/Nette/loader.php";
-require_once $params['libsDir'] . "/.composer/autoload.php";
+require_once $params['libsDir'] . "/autoload.php";
 require_once __DIR__ . "/../Nella/loader.php";
 Nella\SplClassLoader::getInstance()
     ->addNamespaceAlias('NellaTests', __DIR__ . '/cases')
