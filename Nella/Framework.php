195d95f44e1e207aafc8c0d34101618303386ba1
Patrik Votocek <patrik@votocek.cz>
2012-09-11 04:39:55 +0200

    remove Framework::REVISION because this info is in composer.lock

--- Nella/Framework.php
+++ Nella/Framework.php
@@ -4,7 +4,7 @@
  *
  * Copyright (c) 2006, 2012 Patrik Votoček (http://patrik.votocek.cz)
  *
- * For the full copyright and license information, 
+ * For the full copyright and license information,
  * please view the file LICENSE.txt that was distributed with this source code.
  */
 
@@ -19,8 +19,7 @@ final class Framework
 {
 	/**Nella Framework version identification */
 	const NAME = 'Nella Framework',
-		VERSION = '2.0-dev',
-		REVISION = '$WCREV$ released on $WCDATE$';
+		VERSION = '2.0-dev';
 
 	/**
 	 * Static class - cannot be instantiated.
