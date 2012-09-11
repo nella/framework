6471616bbadb75135adc7ac4fc41ca50e96e68ba
Patrik Votocek <patrik@votocek.cz>
2012-09-11 04:29:39 +0200

    tests: running test without build-tools

--- before-build.sh
+++ before-build.sh
@@ -14,15 +14,11 @@ if [ -d "build" ]
 then
 	rm -rf build
 fi
-git clone $BUILD_TOOLS_REPO build
-cd build && git submodule update --init && cd ..
 
 ####################
 # Move build files #
 ####################
 
-mv build/build.xml ./
-mv build/phpunit.xml ./
 mv build/build.sh ./
 
 ################
