1413f59ceeace5ec17989445237d90d7559d687f
Patrik Votocek <patrik@votocek.cz>
2012-09-16 00:21:24 +0200

    reopen dev

--- Nella/Framework.php
+++ Nella/Framework.php
@@ -19,7 +19,7 @@ final class Framework
 {
 	/**Nella Framework version identification */
 	const NAME = 'Nella Framework',
-		VERSION = '2.0-beta1';
+		VERSION = '2.0-dev';
 
 	/**
 	 * Static class - cannot be instantiated.
